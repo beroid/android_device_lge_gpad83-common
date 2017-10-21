507982090af539bbe080e2841558d03bcf2bc78e
Artem Borisov <dedsa2002@gmail.com>
2017-10-21 22:41:19 +0300

    gpad83-common: set BOARD_KERNEL_IMAGE_NAME
    Change-Id: Ia409a8f2e4e0a2cb72f621f7e307d5cc26bb951d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,7 @@ TARGET_KERNEL_SOURCE := kernel/lge/v500
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8 ehci-hcd.park=3 lpj=67677 androidboot.hardware=qcom vmalloc=400M
 BOARD_KERNEL_BASE := 0x80200000
+BOARD_KERNEL_IMAGE_NAME = zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_CUSTOM_BOOTIMG_MK := device/lge/gpad83-common/mkbootimg.mk
