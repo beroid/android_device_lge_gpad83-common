ce0f622f6aef1e771f1818d960e7c19bb748c2f6
fefifofum <jorsellis@gmail.com>
2017-11-05 12:33:02 +0100

    gpad83-common: Update camera dependencies

--- gpad83-common.mk
+++ gpad83-common.mk
@@ -62,7 +62,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
     camera.msm8960 \
-    libcamera_shim \
+    libshims_camera \
     Snap
 
 # Configure libhwui
